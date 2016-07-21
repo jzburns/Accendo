394592ae521bcbdf18ff4549d8fe66320739375a
jburns <jzburns@gmail.com>
2016-07-21 09:13:51 +0100

    new tt script

---
+++ get_tt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ttdir=$HOME/accendo/ttdir
+
+for i in {500..550}
+do
+  wget -O /dev/null --save-cookies cookies.txt  --keep-session-cookies \
+  "http://cmis.it-tallaght.ie/eportal/PortalServ?reqtype=login&username=$i"
+
+  wget -O $ttdir/user"$i".txt --load-cookies cookies.txt \
+  "http://cmis.it-tallaght.ie/eportal/PortalServ?reqtype=timetable&username=$i&action=getgrid&sKey=2015-2016%7C$i&sWeeks=&sType=lecturer&sYear=&sFromDate=&sToDate=&sEventType=&sModOccur=&sSource=&instCode=-2&instName=&useBooking=null&contactkey=&isBooking=null&subTitle="
+
+rm cookies.txt
+
+done
