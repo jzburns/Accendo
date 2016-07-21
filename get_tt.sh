117e4761526ac7d1504372bd6803dfd4d1287123
jburns <jzburns@gmail.com>
2016-07-21 09:40:47 +0100

    added sed parsing of the stream

--- get_tt.sh
+++ get_tt.sh
@@ -7,9 +7,12 @@ do
   wget -O /dev/null --save-cookies cookies.txt  --keep-session-cookies \
   "http://cmis.it-tallaght.ie/eportal/PortalServ?reqtype=login&username=$i"
 
-  wget -O $ttdir/user"$i".txt --load-cookies cookies.txt \
+  wget -O $ttdir/user"$i".html --load-cookies cookies.txt \
   "http://cmis.it-tallaght.ie/eportal/PortalServ?reqtype=timetable&username=$i&action=getgrid&sKey=2015-2016%7C$i&sWeeks=&sType=lecturer&sYear=&sFromDate=&sToDate=&sEventType=&sModOccur=&sSource=&instCode=-2&instName=&useBooking=null&contactkey=&isBooking=null&subTitle="
 
 rm cookies.txt
+
+sed -e 's/<[^>]*>//g' $ttdir/user"$i".html > $ttdir/user"$i".txt
+rm $ttdir/user"$i".html
 
 done
